1f140cd9d5d972077c222adb3d1c66ff33092165
nguyenphuocvu <vunguyenphuoc2906@gmail.com>
2025-02-28 14:46:42 +0700

    crud-trello

--- Crud-Trello/src/routes/index.js
+++ Crud-Trello/src/routes/index.js
@@ -3,10 +3,12 @@ const trelloRouter = require('./trello')
 const cardRouter = require('./card')
 
 
+
 function route(app) {
     
     app.use('/', trelloRouter)
     app.use('/cards', cardRouter)
+ 
  
 }
 
