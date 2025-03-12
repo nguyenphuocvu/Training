5f08eb036292251b7c7e7af72a5490ad1d4d6321
nguyenphuocvu <vunguyenphuoc2906@gmail.com>
2025-03-12 09:27:50 +0700

    done index.js server

--- Crud-Trello/server/src/index.js
+++ Crud-Trello/server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cors = require('cors'); // Thêm CORS
 require('dotenv').config();
+const cors = require('cors'); 
+
 
 const connectDB = require('./config/db'); 
 const route = require('./routes'); 
@@ -8,10 +9,10 @@ const route = require('./routes');
 const app = express();
 const port = process.env.PORT || 3030; 
 
-// Middleware
-app.use(cors({ origin: '*', methods: 'GET,POST,DELETE,PATCH' })); // Cho phép mọi origin
+// Middleware xử lý JSON
+app.use(cors()); 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // Hỗ trợ JSON payload
+app.use(express.json()); 
 
 // Kết nối database
 connectDB();
@@ -19,10 +20,6 @@ connectDB();
 // Routes
 route(app);
 
-// Trả về phản hồi cho OPTIONS để tránh Preflight Request
-app.options('*', (req, res) => {
-    res.sendStatus(204);
-});
 
 // Chạy server
 async function main() {
